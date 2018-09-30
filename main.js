0cd7ebcfb236f13592b0bf16cf31c659111eb3bd
Julian Nicholls <juliannicholls29@gmail.com>
2018-09-30 14:26:32 +0100

    Make Pi page brighter

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', init);
 const words = ['Developer', 'Maker'];
 
 class Typewriter {
-  constructor () {
+  constructor() {
     this.holder = document.getElementById('text-holder');
     this.words = words;
     this.listIndex = 0;
@@ -11,7 +11,7 @@ class Typewriter {
     this.deleting = false;
   }
 
-  type () {
+  type() {
     const { deleting, words, listIndex, wordIndex, holder } = this;
     let delay = 300;
 
@@ -31,14 +31,14 @@ class Typewriter {
       delay = 3000;
     }
 
-    const text = words[listIndex].substr(0, wordIndex);
+    const text = wordIndex > 0 ? words[listIndex].substr(0, wordIndex) : ' ';
     holder.innerHTML = `<span class="text">${text}</span>`;
 
     setTimeout(() => this.type(), delay);
   }
 }
 
-function init () {
+function init() {
   const tw = new Typewriter();
 
   tw.type();
