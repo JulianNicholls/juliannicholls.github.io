71d6145ca64c361a2a827a56900b780f016b46d3
Julian Nicholls <juliannicholls29@gmail.com>
2019-11-22 21:03:48 +0000

    First try of roadworks

---
+++ roadworks/precache-manifest.96dc2ca57ac06f77a0044f86ae3d9c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38143891027b24c53eb5c8ea7f67930e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6146352d02679518c3b6",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8af36ed9733ac79342de",
+    "url": "/static/css/main.1e640dfa.chunk.css"
+  },
+  {
+    "revision": "6146352d02679518c3b6",
+    "url": "/static/js/2.b8d74653.chunk.js"
+  },
+  {
+    "revision": "8af36ed9733ac79342de",
+    "url": "/static/js/main.7c3d0a14.chunk.js"
+  },
+  {
+    "revision": "9e77861bbe94994019cb",
+    "url": "/static/js/runtime-main.69db7ab4.js"
+  }
+]);
